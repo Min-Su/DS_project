ac0424bb9966a63a96489d4246a78d94eec0c60a
karosis28 <karosis28@gmail.com>
2015-06-09 15:28:27 +0900

    Add UImanager.h, UImanager.cpp file

---
+++ data_structure/data_structure/UImanager.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+#include"UImanager.h"
+
+
+UImanager::UImanager() {
+	this->input = 0;
+}
+
+void UImanager::input_Number() {
+	cin >> this->input;
+}
+
+void UImanager::menu() {
+	cout << "1. Stack" << endl;
+	cout << "2. List" << endl;
+	cout << "3. Que" << endl;
+	cout << "4. Tree" << endl;
+	cout << "5. Exit" << endl;
+	cout << "Select the Number >>";
+}
+
+void UImanager::run() {
+	while(1) {
+		menu();
+		input_Number();
+
+		switch(this->input) {
+			case 1 : 
+				break;
+
+			case 2 :
+				break;
+
+			case 3 :
+				break;
+
+			case 4 :
+				break;
+
+			case 5 :
+				exit(1);
+				break;
+
+			default:
+				cout << "Input is unavailable" << endl;
+				break;
+		}
+	}
+}
---
+++ data_structure/data_structure/UImanager.h
@@ -0,0 +1,14 @@
+#ifndef __UIMANAGER_H__
+#define __UIMANAGER_H__
+
+class UImanager {
+private:
+	int input;
+public:
+	UImanager();
+	void input_Number();
+	void menu();
+	void run();
+};
+
+#endif
